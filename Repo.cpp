d601c3e42f57049564f00e8ab8bb14cfabe6f1b1
Diego <diegoinsano156@gmail.com>
2024-10-15 16:20:52 +0200

    Version 1 del codigo

---
+++ Repo.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    double D, x1, x2; // Usamos double para las raíces
+
+    // Leer los coeficientes
+    cin >> A >> B >> C;
+
+    // Calcular el discriminante
+    D = (B * B) - (4 * A * C);
+
+    x1 = (-B + sqrt(D)) / (2 * A); // Raíz 1
+    x2 = (-B - sqrt(D)) / (2 * A); // Raíz 2
+
+    // Mostrar las raíces
+    cout << x1 << " " << x2 << endl;
+
+    return 0;
+}
